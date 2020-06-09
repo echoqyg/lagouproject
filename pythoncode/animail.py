83ac6282b5f841f15a4abbb4af55a0640d5097d1
xiaohui <296891196@qq.com>
2020-06-09 21:26:33 +0800

    python实战二作业 加入yaml配置文件，修改init和调用方法

--- pythoncode/animail.py
+++ pythoncode/animail.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import yaml
 
 
 class Animal:
     name = ""
     color = ""
     age = ""
-    sex = ""
+    gender = ""
 
-    def __init__(self, name, color, age, sex):
+    def __init__(self, name, color, age, gender):
         self.name = name
         self.color = color
         self.age = age
-        self.sex = sex
+        self.gender = gender
 
     def call(self):
         print("会叫")
@@ -22,44 +23,46 @@ class Animal:
 
 
 class cat(Animal):
-    hair = "短毛"
+    hair = ""
 
-    def __init__(self, name, color, age, sex):
-        self.name = name
-        self.color = color
-        self.age = age
-        self.sex = sex
+    def __init__(self, kwargs):
+        self.name = kwargs["name"]
+        self.color = kwargs["color"]
+        self.age = kwargs["age"]
+        self.gender = kwargs["gender"]
+        self.hair = kwargs["hair"]
 
     def call(self):
         print(f"{self.name}会喵喵叫")
 
     def ability(self):
-        print(f"猫猫叫{self.name}, 颜色是{self.color}，年龄是{self.age}，性别是{self.sex}，毛发为{self.hair}, 捉到了老鼠")
+        print(f"猫猫叫{self.name}, 颜色是{self.color}，年龄是{self.age}，性别是{self.gender}，毛发为{self.hair}, 捉到了老鼠")
 
 
 class dog(Animal):
-    hair = "长毛"
+    hair = ""
 
-    def __init__(self, name, color, age, sex):
-        self.name = name
-        self.color = color
-        self.age = age
-        self.sex = sex
+    def __init__(self, kwargs):
+        self.name = kwargs["name"]
+        self.color = kwargs["color"]
+        self.age = kwargs["age"]
+        self.gender = kwargs["gender"]
+        self.hair = kwargs["hair"]
 
     def call(self):
         print(f"{self.name}会汪汪叫")
 
     def ability(self):
-        print(f"狗狗叫{self.name}, 颜色是{self.color}，年龄是{self.age}，性别是{self.sex}，毛发为{self.hair}")
+        print(f"狗狗叫{self.name}, 颜色是{self.color}，年龄是{self.age}，性别是{self.gender}，毛发为{self.hair}")
         print(f"{self.name}会看家")
 
 
-a = cat("红", "白色", "3岁", "母")
-a.ability()
-a.call()
-b = dog("黑", "白色", "3岁", "母")
-b.ability()
-b.call()
+# a = cat("红", "白色", "3岁", "母")
+# a.ability()
+# a.call()
+# b = dog("黑", "白色", "3岁", "母")
+# b.ability()
+# b.call()
 
 """ 
 打印内容为
@@ -69,3 +72,11 @@ b.call()
 黑会看家
 黑会汪汪叫
 """
+
+zoo = yaml.load(open("animail.yml"), Loader=yaml.FullLoader)
+a = cat(zoo["cat"])
+a.ability()
+a.call()
+b = dog(zoo["dog"])
+b.ability()
+b.call()
